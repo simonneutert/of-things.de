6b3e771059dce2035b7ba222ce423918f617e253
Simon Neutert <simonneutert@users.noreply.github.com>
2023-05-10 15:27:51 +0200

    about/footer

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
     <p class="text-center">Check out the code of the project on <a href="https://github.com/simonneutert/of-things.de" target="_blank">GitHub</a></p>
     <hr>
     <p class="footer-text text-center">Styled with <a href="http://foundation.zurb.com" target="_blank">foundation</a></p>
-    <p class="footer-text text-center">Imprint: <a href="http://www.simon-neutert.de">Simon Neutert</a>, Südring 311, 55128 Mainz</p>
+    <p class="footer-text text-center">Imprint: <a href="http://www.simon-neutert.de">
+      Simon Neutert<br />Postfach 1120<br />55258 Ingelheim
+    </p>
   </footer>
 </html>
